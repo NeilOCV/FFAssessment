2172a3c770c7480c993d68c2eafa43bd6abeeac6
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-09-10 20:31:30 +0200

    201709102031
    Basic connection to the web server established.

---
+++ FFAssessment/APIModels/CustomersEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIMessages
+{
+    public class CustonmersEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal latitude { get; set; }
+        public decimal longetude { get; set; }
+    }
+}
---
+++ FFAssessment/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using APIMessages;
+using System.Net.Http;
+
+namespace FFAssessment.Controllers
+{
+    public class CustomersController : Controller
+    {
+        const string strBaseURL = "http://localhost:20564/";
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomersFromAPI();
+
+            return View(customers);
+        }
+
+        private List<CustonmersEntity> GetCustomersFromAPI()
+        {
+            try
+            {
+                var resultList = new List<CustonmersEntity>();
+                var client = new HttpClient();
+                var getDataTask = client.GetAsync(strBaseURL + "api/Customers")
+                    .ContinueWith(response =>
+                    {
+                        var result = response.Result;
+                        if(result.StatusCode==System.Net.HttpStatusCode.OK)
+                        {
+                            var readResult = result.Content.ReadAsAsync<List<CustonmersEntity>>();
+                            readResult.Wait();
+                            resultList = readResult.Result;
+                        }
+                    });
+                getDataTask.Wait();
+                return resultList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
