bda41679af686d30d875990651415b953f55b22e
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-09-10 19:09:20 +0200

    201709101908
    Initial check-in

---
+++ FFAssessment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FFAssessment.MvcApplication" Language="C#" %>
---
+++ FFAssessment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FFAssessment.Startup))]
+namespace FFAssessment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
