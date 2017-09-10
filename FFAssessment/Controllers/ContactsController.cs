25923e43e72fa2a554653f7b438f4f78319cc44e
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-09-10 20:54:49 +0200

    201709102054
    More models and views (Contact)

---
+++ FFAssessment/Controllers/ContactsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using APIMessages;
+using System.Net.Http;
+
+namespace FFAssessment.Controllers
+{
+    public class ContactsController : Controller
+    {
+        const string strBaseURL = "http://localhost:20564/";
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            var contacts = GetContactsFromAPI();
+
+            return View(contacts);
+        }
+        private List<ContactEntity> GetContactsFromAPI()
+        {
+            try
+            {
+                var resultList = new List<ContactEntity>();
+                var client = new HttpClient();
+                var getDataTask = client.GetAsync(strBaseURL + "api/Contacts")
+                    .ContinueWith(response =>
+                    {
+                        var result = response.Result;
+                        if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            var readResult = result.Content.ReadAsAsync<List<ContactEntity>>();
+                            readResult.Wait();
+                            resultList = readResult.Result;
+                        }
+                    });
+                getDataTask.Wait();
+                return resultList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
